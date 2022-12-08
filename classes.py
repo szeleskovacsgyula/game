4400d3ca135a6561c12876fe8bac716e90bbddd9
Gyula Széles-Kovács <49909562+szeleskovacsgyula@users.noreply.github.com>
2022-12-08 09:25:15 +0100

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,23 @@
+class Base:
+    name = ""
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Warrior(Base): 
+    name = "Warrior"
+    bonusAtk = 10
+    bonusArmor = 15
+    bonusHp = 50
+
+class Mage: 
+    name = "Mage"
+    bonusAtk = 30
+    bonusArmor = 0
+    bonusMana = 100
+
+class Archer: 
+    name = "Archer"
+    bonusAtk = 25
+    bonusArmor = 0
+    bonusHP=25
